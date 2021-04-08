dd6d5581a6f6473fef2cbb2e021244e644adad6a
Paul Kim <jkim926@aucklanduni.ac.nz>
2021-04-09 11:57:57 +1200

    Update neural network.py

--- neural network.py
+++ neural network.py
@@ -1,16 +1,3 @@
-import torch
-import torchvision
-from torchvision import transforms, datasets
-
-train = datasets.MNIST("", train=True, download=True, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
-
-
-test = datasets.MNIST("", train=False, download=True, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
-    
-
-trainset = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True)
-testset = torch.utils.data.DataLoader(test, batch_size=32, shuffle=True)
-
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
