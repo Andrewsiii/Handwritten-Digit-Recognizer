9e5bbd13dbb1bc736e626fd2587d99dbf8efe298
Paul Kim <jkim926@aucklanduni.ac.nz>
2021-04-09 12:17:24 +1200

    Update mnist training.py
    training settings and initialisations complete

--- mnist training.py
+++ mnist training.py
@@ -1,12 +1,36 @@
 import torch
 import torchvision
-from torchvision import transforms, datasets
+from __future__ import print_function
+from torch import nn, optim, cuda
+from torch.utils import data
+from torchvision import datasets, transforms
+import torch.nn.functional as F
+import time
 
-train = datasets.MNIST("", train=True, download=True, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
+#training settings
+batch_size = 32
+device = 'cuda' if cuda.is_available() else 'cpu'
+print(f'Training MNIST Model on {device}\n{"=" * 44}')
 
 
-test = datasets.MNIST("", train=False, download=True, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
+#Mnist dataset
+trainset = datasets.MNIST("", train=True, download=True, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
+
+
+testset = datasets.MNIST("", train=False, download=True, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
     
 
-trainset = torch.utils.data.DataLoader(train, batch_size=32, shuffle=True)
-testset = torch.utils.data.DataLoader(test, batch_size=32, shuffle=True)
+#Data loader
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+
+testloader = torch.utils.data.DataLoader(testset, batch_size= batch_size, shuffle=True)
+
+
+#initialisations
+model = Net()
+model.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+#training
+
