cc41aa6cf32354eff88e561571b374954a0fc1e6
asio896 <68887738+asio896@users.noreply.github.com>
2021-04-15 21:52:02 +1200

    Update Project_Gui.py

--- Project_Code/Project_Gui.py
+++ Project_Code/Project_Gui.py
@@ -1,13 +1,36 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QMessageBox, QDialog, QGridLayout, QPushButton, QLineEdit, QTextEdit
-from PyQt5.QtCore import QCoreApplication
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp, QMessageBox, QDialog, QGridLayout, QPushButton, QLineEdit, QTextEdit, QLabel,QVBoxLayout
+from PyQt5.QtCore import QCoreApplication, Qt
+from PyQt5.QtGui import QIcon, QPen, QPixmap, QPainter
 
+class CanvasWindow(QMainWindow):
+        def __init__(self,parent = None):
+            super(CanvasWindow,self).__init__(parent,Qt.Window)
+            self.label = QLabel()
+            canvas = QPixmap(500,500)
+            canvas.fill(Qt.black)
+            self.label.setPixmap(canvas)
+
+            self.setCentralWidget(self.label)
+            vbox = QVBoxLayout()
+            vbox.addWidget(self.label)
+            self.setLayout(vbox)
+            self.setWindowTitle('Canvas')
+            self.setGeometry(300, 200, 800, 600)
+           
+
+
+        def mouseMoveEvent(self, e):
+            painter = QPainter(self.label.pixmap())
+            painter.setPen(QPen(Qt.white,  20))
+            painter.drawPoint(e.x(), e.y())
+            painter.end()
+            self.update()
 
 class MyApp(QMainWindow):
 
-    def __init__(self):
-        super().__init__()
+    def __init__(self,parent=None):
+        super(MyApp,self).__init__(parent)
         self.initUI()
 
     def initUI(self):
@@ -28,6 +51,11 @@ class MyApp(QMainWindow):
         viewTestingImages = QAction ('View Testing Images', self)
         viewTestingImages.setStatusTip('View the testing images')
 
+        DrawingCanvas = QAction('Drawing Canvas',self)
+        DrawingCanvas.setStatusTip('View the testing images')
+        self.newwindow = CanvasWindow(self)
+        DrawingCanvas.triggered.connect(self.CanvasClk)
+
         
 
         self.statusBar().showMessage('Ready')
@@ -36,14 +64,12 @@ class MyApp(QMainWindow):
         filemenu = menubar.addMenu('&File')
         filemenu.addAction(TrainModel)
         filemenu.addAction(exitAction)
+        filemenu.addAction(DrawingCanvas)
         viewmenu = menubar.addMenu('&View')
         viewmenu.addAction(viewTrainingImages)
         viewmenu.addAction(viewTestingImages)
-       
-       
-
         self.setWindowTitle('Handwritten Digit Recognizer')
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(300, 200, 800, 600)
         self.show()
 
     def OpenWindow(self):
@@ -53,9 +79,6 @@ class MyApp(QMainWindow):
         grid = QGridLayout()
         dialog.setLayout(grid)
         btn1 = QPushButton('&Download MNIST', self)
-        
-        
-
         btn2 = QPushButton(self)
         btn2.setText('Train')
 
@@ -66,8 +89,10 @@ class MyApp(QMainWindow):
         grid.addWidget(btn2,3,2)
         grid.addWidget(btn3,3,3)
         dialog.show()
-        
-        
+    def CanvasClk(self):
+        self.newwindow.show()
+
+    
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
