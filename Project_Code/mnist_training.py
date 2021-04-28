77a9b5c29c9f155f609c5d07d503289a5338f32b
Paul Kim <jkim926@aucklanduni.ac.nz>
2021-04-28 14:41:58 +1200

    comments added

--- Project_Code/mnist_training.py
+++ Project_Code/mnist_training.py
@@ -15,11 +15,15 @@ from skimage import data, io, filters
 from PIL import Image, ImageOps
 import os
 import Project_Gui
+from neural_network import LeNet5
+
+
 #training settings
 batch_size = 64 
 
 
-#Mnist dataset
+#Data handling
+#Downloading, transforming into tensor and loading the MNSIT dataset shuffled into the loader
 def Initial():
     trainset = datasets.MNIST(root='', train=True, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
 
@@ -30,16 +34,17 @@ def Initial():
     testloader = torch.utils.data.DataLoader(testset, batch_size= batch_size, shuffle=True)
 
 
-from neural_network import LeNet5
-
-
-#initialisations
+#model initialisations
 model = LeNet5()
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.5)
 
+
+
 #training function
+#Batches of the MNIST data that is loaded into the trainset is passed through the model and the weights are adjusted using the back propagation function.
+#loss is also calculated and displayed together with the progress of the training in a print function
 def train(epoch):
     trainset = datasets.MNIST(root='', train=True, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
 
@@ -56,12 +61,14 @@ def train(epoch):
         loss = criterion(output, target)
         loss.backward()
         optimizer.step()
-        if batch_idx % 10 == 0:
+        if batch_idx % 10 == 0:    #display progress of training in each epoch
             print('Train Epoch: {} | Batch Status: {}/{} ({:.0f}%) | Loss: {:.6f}'.format(
                 epoch, batch_idx * len(data), len(trainloader.dataset),
                 100. * batch_idx / len(trainloader), loss.item()))   
 
 #testing function
+#passes the testset MNIST data through the model while model is in Evaluation mode so weights are not adjusted
+#Loss is calculated together with whether the model predicted correct
 def test():
     trainset = datasets.MNIST(root='', train=True, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
 
@@ -84,34 +91,39 @@ def test():
 
 
 
-    test_loss /= len(testloader.dataset)
+    test_loss /= len(testloader.dataset)    # Loss and number of correct predictions are displayed together with model accuracy for that test in print function
     print(f'===========================\nTest set: Average loss: {test_loss:.4f}, Accuracy: {correct}/{len(testloader.dataset)} '
           f'({100. * correct / len(testloader.dataset):.0f}%)')
 
 
-working_dir = os.getcwd()
+working_dir = os.getcwd()   #defining the working directory
 data_dir = (working_dir + '\\testdata')
-test_transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])
+test_transforms = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))]) #transform to tensor function.
 
+
+#function that opens and processes an image so that it can pass through the model for recognition
 def open_image(directory):
 
     image = Image.open(directory)
 
-    image = ImageOps.grayscale(image)
-    image = np.array(image)
-    image = resize((image), (28,28), anti_aliasing= True)
+    image = ImageOps.grayscale(image) #transform to grayscale image
+    image = np.array(image)             #transform to numpy array
+    image = resize((image), (28,28), anti_aliasing= True)   #process the image to a 28x28 image with anti aliasing 
 
 
 
     image = test_transforms(image).float()
-    image = image.unsqueeze(1)
+    image = image.unsqueeze(1)              #unsqueeze image dim 1
 
     return image
 
+
+#loading the pretrained model
 def Loading():
     model.load_state_dict(torch.load('model_weights.pth'))
 
-#running the training
+
+#running the training for 10 epochs and evaluating after each epoch to display model accuracy during training.
 def training_and_testing():
     model.eval()
     for epoch in range (1, 11):
@@ -119,28 +131,21 @@ def training_and_testing():
         test()
 
 
+#function that takes a processed image as an input and passes it through the model
+#returns the classes on the x-axis, probabilites of prediction on y-axis and the most dominant class as the 'index'
 def prediction(image):
     model.eval()
     output = model(image)
 
     index = output.data.cpu().numpy().argmax()
-    #print(index)
-
-
-
+   
     probabilities = torch.exp(output)
-    #print(probabilities)
-
-    
-    #output = output.detach().numpy()
-    #print(probabilities)
+ 
     probabilities = probabilities.detach().numpy()
     x = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-    # x = np.arange(len(x_label))
-    # y = list(probabilities)
+
     
-    #print(probabilities)
-    
+    #transforming the tensor of probabilties from the softmax output to a numpy array so it can be represented on a matplotlib graph
     y = np.append(probabilities[0,[0]], probabilities[0,[1]])
     y = np.append(y, probabilities[0,[2]])
     y = np.append(y, probabilities[0,[3]])
@@ -158,21 +163,26 @@ def prediction(image):
     #plt.show()
     return x, y, index
 
+#function that cycles through and saves the MNIST training dataset as an individual pillow image.
 def viewTrainDataset(i):
     trainset = datasets.MNIST(root='', train=True, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
     
     y, _ = trainset[i]
-    to_pil = transforms.ToPILImage()
+    to_pil = transforms.ToPILImage() #function that transform tensor to pillow image
     image = to_pil(y)
     image.save('training_image.png')
 
+
+#function that cycles through and saves the MNIST testing dataset as an individual pillow image.
 def viewTestDataset(j):
     testset = datasets.MNIST(root='', train=False, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
     
     x, _ = testset[j]
-    to_pil = transforms.ToPILImage()
+    to_pil = transforms.ToPILImage() #function that transform tensor to pillow image
     im = to_pil(x)
     im.save('testing_image.png')
+
+#function that connects to train button in GUI which trains the model and saves the trained model.
 def TrainingButton():
     model.eval()
     test()
@@ -180,20 +190,15 @@ def TrainingButton():
     torch.save(model.state_dict(), 'model_weights.pth')
 
 
-# trainset = datasets.MNIST(root='', train=True, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=False)
-# print(trainloader.dataset.targets[0])
-
-
-#testing function
+#function used to calculate and display the precision, recall and F1 score for the trained model.
 def analysis():
     testset = datasets.MNIST(root='', train=False, download=False, transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307),(0.3081))])) 
     testloader = torch.utils.data.DataLoader(testset, batch_size= 1, shuffle=True)
     model.eval()
 
-    count0 = 0
-    count1 = 0
-    total = 0
+    count0 = 0  #True positive + False positive count
+    count1 = 0  #true positive count
+    total = 0   #True positive + False negative count
     for data, target in testloader:
         output = model(data)
 
@@ -202,7 +207,7 @@ def analysis():
 
         predict = output.data.cpu().numpy().argmax()
         
-        #print(predict)
+        #loop that extracts the true positive, false positive and false negative values for a single class
         if predict == 9:
             count0 = count0 + 1
             if pred.eq(target.data.view_as(pred)).cpu().sum():
@@ -210,7 +215,7 @@ def analysis():
         if target.data.numpy() == [9]:
             total = total + 1
 
-
+    #Calculation of the statistical analysis values.
     precision = (count1/count0)
     recall = count1/total
     f1 = (2*precision*recall)/(precision+recall)
@@ -219,6 +224,3 @@ def analysis():
     print(recall)
     print(f1)
 
-
-Loading()
-test()
--- Project_Code/neural_network.py
+++ Project_Code/neural_network.py
@@ -4,21 +4,19 @@ import numpy as np
 import torch.optim as optim
 
 class LeNet5(nn.Module):
-    def __init__(self):
+    def __init__(self):                 # function that defines the layers and its parameters in the neural network
         super(LeNet5, self).__init__()
-        self.conv1 = nn.Conv2d(1, 16, 5, bias = False)
-        self.conv2 = nn.Conv2d(16, 32, 5, bias = False)
-        self.fc1 = nn.Linear(512, 256, bias = False)
-        self.fc2 = nn.Linear(256, 64, bias = False)
-        self.fc3 = nn.Linear(64, 10, bias = False)
-        self.relu = nn.ReLU()
-        self.dropout = nn.Dropout(0.25)
-        self.batchnorm1 = nn.BatchNorm2d(16)
-        self.batchnorm2 = nn.BatchNorm2d(32)
-        self.avepooling = nn.AvgPool2d(2)
-        self.softmax = nn.LogSoftmax(-1)
+        self.conv1 = nn.Conv2d(1, 16, 5, bias = False)  #first convolutional layer that takes 1 input and returns 16 outputs. consists of a 5x5 kernel with no biasing
+        self.conv2 = nn.Conv2d(16, 32, 5, bias = False) #second convolutional layer that takes 16 input and returns 32 outputs. consists of a 5x5 kernel with no biasing
+        self.fc1 = nn.Linear(512, 256, bias = False)    #first fully connected layer that takes 512 inputs and returns 256 outputs with no biasing
+        self.fc2 = nn.Linear(256, 64, bias = False)     #second fully connected layer that takes 256 inputs and returns 64 outputs with no biasing
+        self.fc3 = nn.Linear(64, 10, bias = False)      #final fully connected layer that takes 64 inputs and returns 10 outputs matching the 10 output classes with no biasing
+        self.relu = nn.ReLU()               #Relu activation function
+        self.dropout = nn.Dropout(0.25)     #dropout layer that drops out 25% of the neurons
+        self.avepooling = nn.AvgPool2d(2)   #2D averaging pooling layer
+        self.softmax = nn.LogSoftmax(-1)    #Logsoftmax activation function that returns the probabilty distribution of the outcome
 
-    def forward(self, input):
+    def forward(self, input):           #function that displays the architecture of the model
         out = self.conv1(input)
         out = self.relu(out)
         out = self.avepooling(out)
@@ -28,12 +26,11 @@ class LeNet5(nn.Module):
         out = self.dropout(out)
         out = self.avepooling(out)
 
-        out = out.view(out.size(0), -1)
+        out = out.view(out.size(0), -1)     #flattening the data so it can be passed through the fully connected linear layers.
 
         out = self.fc1(out)
         out = self.relu(out)
 
-        #out = self.dropout(out)
 
         out  = self.fc2(out)
         out = self.relu(out)
